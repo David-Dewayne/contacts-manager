d824d6ce85d4900ddfcb3552835cce76dd5066c0
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-07-27 10:49:50 -0500

    Base lines drawn for PHP code

--- contactsManager.php
+++ contactsManager.php
@@ -1,1 +1,46 @@
 <?php
+$true = false;
+//Continue the program untill user calls 5:exit
+do{
+	//Get our contact info
+	$filename = 'contacts.txt';
+	$handle =  fopen($filename, 'r');
+	$contents = fread($handle, filesize($filename));
+	var_dump($contents);
+	fclose($handle);
+	//get user input
+	$guess = fwrite(STDIN, PHP_EOL);
+	$guess = explode(" ", $guess);
+	//user input fuctionality
+	switch ($guess[0]) {
+		//Display all contacts
+		case '1':
+			fwrite(STDOUT, $contents);
+		break;
+		//Add a contact
+		case '2':
+			$handle =  fopen($filename, 'a');
+			fwrite($handle, $guess[1]."|".$guess[2].PHP_EOL);
+			fclose($handle);
+		break;
+		//Search for a contact
+		case '3':
+			$var = array_search($guess[1], $contents);
+			fwrite(STDOUT, $contents[$var]);
+		break;
+		//Delete a contact
+		case '4':
+			
+		break;
+		//Exit
+		case '5':
+			$true = true;
+		break;
+		case'help':
+			fwrite(STDOUT, "1:Display All" .PHP_EOL."2:Add a contact".PHP_EOL."3:Search for a contact".PHP_EOL."4:Delete A contact".PHP_EOL."5:exit the program");
+		break;
+		default:
+			fwrite(STDOUT, "First character needs to be a number between 1-5!");
+			break;
+	}
+}while($true);
